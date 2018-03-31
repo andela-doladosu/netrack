b95d83a36f728ab61bb14396aef99284fc8cd175
Dara Oladosu <daradosu@gmail.com>
2018-03-31 15:22:58 +0100

    test routes require api token

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -4,9 +4,12 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class ApiTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * Test all responses are in JSON
      *
@@ -22,5 +25,31 @@ class ApiTest extends TestCase
                 'message' => 'Unauthenticated.'
             ]
         );
+    }
+
+    /**
+     * Test Protected Routes Require Api Token
+     *
+     * @return void
+     */
+    public function testProtectedRoutesRequireApiToken()
+    {
+        $response = $this->get('/api/user');
+        $response->assertStatus(401);
+
+        $token = str_random(20);
+        factory(User::class)->create(
+            [
+                'api_token' => $token
+            ]
+        );
+
+        $response = $this->get(
+            '/api/user',
+            [
+                'HTTP_Authorization' => 'Bearer ' . $token
+            ]
+        );
+        $response->assertStatus(200);
     }
 }
